e015e3c34c755673044537601b27b1a5eea68104
Matt Mawhinney <matt.mawhinney@gmail.com>
2013-10-17 11:41:42 -0500

    devise up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TwitterTrending::Application.routes.draw do
-  root 'static_pages#home'
+  devise_for :users
+  root :to => 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
